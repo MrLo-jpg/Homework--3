29ae9537b1f6ac57142975d198c83c84daebeb2f
MrLo-jpg <lsa-71@mail.ru>
2024-09-01 23:52:11 +0300

    Done!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int clientOS = 1;
+        int clientOS = 0;
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 0) {
+        } else {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
         System.out.println();
